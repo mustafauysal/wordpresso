8e65917499f5d1cda545736e196b83464bea610b
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2013-12-06 15:20:22 +0200

    char convert için html special char table kullanıldı, başka karakterlerin de hatalı gelme ihitmaline karşın. mustafauysal'ın #1 nolu bildirimi[öneri] üzerine include yerine get_template_part() kullanıldı

--- functions.php
+++ functions.php
@@ -1,11 +1,17 @@
 <?php
-
 // 4sq map gorsel linkleri arasindaki x degerinin oldugu gibi kalmasi icin
 // tesekkurler @aligundogdu
 
-add_filter("the_title","mapStringConvert");
-function mapStringConvert($url){
-    $s = array("&#215;","&#038;","×");
-    $d = array("x","&","x");
-    return str_replace($s,$d,$url);
+add_filter("the_title","unhtmlentities");
+
+function unhtmlentities ($string)  {
+    $trans_tbl = get_html_translation_table (HTML_ENTITIES);
+    $trans_tbl = array_flip ($trans_tbl);
+    $ret = strtr ($string, $trans_tbl);
+    return  preg_replace('/\&\#([0-9]+)\;/me', "chr('\\1')",$ret);
+}
+
+function get_section($section_name)
+{
+    return  get_template_part('pages/Section' . $section_name);
 }
--- index.php
+++ index.php
@@ -3,25 +3,25 @@
 <ol class="Sections">
 
 	<li id="kim" class="Section SectionCover">
-		<?php include 'pages/SectionCover.php'; ?>
+		<?php get_section('Cover'); ?>
 	</li>
 	<!-- /#blog/.Section/.SectionCover -->
 
 
 	<li id="blog" class="Section SectionBlog">
-		<?php include 'pages/SectionBlog.php'; ?>
+		<?php  get_section('Blog'); ?>
 	</li>
 	<!-- /#blog/.Section/.SectionBlog -->
 
 
 	<li id="lab" class="Section SectionLab">
-		<?php include 'pages/SectionLab.php'; ?>
+		<?php  get_section('Lab'); ?>
 	</li>
 	<!-- /#lab/.Section/.SectionLab -->
 
 
 	<li id="activity" class="Section SectionActivity">
-		<?php include 'pages/SectionActivity.php'; ?>
+		<?php  get_section('Activity'); ?>
 	</li>
 	<!-- /#lab/.Section/.SectionActivity -->
 

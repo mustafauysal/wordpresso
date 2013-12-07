87eaceb8286441337d9136e1c58e7ecd78bb17af
Adem ilter <ademilter@gmail.com>
2013-12-07 02:22:49 +0200

    typekit silindi google font eklendi

--- header.php
+++ header.php
@@ -40,38 +40,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-	<script type="text/javascript">
-		(function () {
-			var config = {
-				kitId        : 'wak3pin',
-				scriptTimeout: 3000
-			};
-			var h = document.getElementsByTagName("html")[0];
-			h.className += " wf-loading";
-			var t = setTimeout(function () {
-				h.className = h.className.replace(/(\s|^)wf-loading(\s|$)/g, " ");
-				h.className += " wf-inactive"
-			}, config.scriptTimeout);
-			var tk = document.createElement("script"),
-					d = false;
-			tk.src = '//use.typekit.net/' + config.kitId + '.js';
-			tk.type = "text/javascript";
-			tk.async = "true";
-			tk.onload = tk.onreadystatechange = function () {
-				var a = this.readyState;
-				if (d || a && a != "complete" && a != "loaded") return;
-				d = true;
-				clearTimeout(t);
-				try {
-					Typekit.load(config)
-				} catch (b) {
-				}
-			};
-			var s = document.getElementsByTagName("script")[0];
-			s.parentNode.insertBefore(tk, s)
-		})();
-	</script>
-
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700|PT+Serif:400,700,400italic,700italic&subset=latin-ext' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.min.css">
 </head>

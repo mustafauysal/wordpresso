601aca09aee66418688ce346e5b3143bd1fae081
mustafauysal <m_uysl@hotmail.com>
2013-12-09 02:10:41 +0200

    wordpresso-main.php güncellendi

--- admin/wordpresso-main.php
+++ admin/wordpresso-main.php
@@ -1,62 +1,62 @@
 <?php
-    if(isset($_POST['save_wordpresso_settings']))
-    {
-        update_option('wordpresso_google_analytics_track_code', $_POST['wordpresso_google_analytics_track_code']);
-    ?>
-    <div id="message" class="updated settings-error"> 
-        <p><strong>Ayarlar kaydedildi.</strong></p>
-    </div>
-    <?php
-    }      
+if ( isset( $_POST['save_wordpresso_settings'] ) ) {
+	update_option( 'wordpresso_google_analytics_track_code', $_POST['wordpresso_google_analytics_track_code'] );
+	?>
+	<div id="message" class="updated settings-error">
+		<p><strong><?php _e( 'Ayarlar kaydedildi.', 'WordPresso' ); ?></strong></p>
+	</div>
+<?php
+}
 ?>
 
 <div id="welcome-panel" class="welcome-panel">
-    <div class="welcome-panel-content">
-        <h3>WordPresso’ya hoşgeldiniz!</h3>
-        <p class="about-description">Başlamanız için bir kaç bağlantıyı bir araya getirdik.</p>
-    </div>
+	<div class="welcome-panel-content">
+		<h3><?php _e( 'WordPresso’ya hoşgeldiniz!', 'WordPresso' ); ?></h3>
+
+		<p class="about-description"><?php _e( 'Başlamanız için bir kaç bağlantıyı bir araya getirdik.', 'WordPresso' ); ?></p>
+	</div>
 </div>
 
-<form method="POST" action="">  
-    <table class="form-table">
-        <tr valign="top">
-            <th scope="row">
-                <label for="wordpresso_google_analytics_track_code">Google Analytics Code</label>
-            </th>
-            <td>
-                <input type="text" name="wordpresso_google_analytics_track_code" id="wordpresso_google_analytics_track_code" placeholder="UA-17768654-1" value="<?php echo get_option('wordpresso_google_analytics_track_code'); ?>" class="regular-text"/>
-            </td>
-        </tr>
-        <tr valign="top">
-            <th scope="row">
-                <label for="">Blog sayfa başlığı</label>
-            </th>
-            <td>
-                <input type="text" name="" id="" placeholder="BLOG" value="" class="regular-text"/>
-            </td>
-        </tr>
-        <tr valign="top">
-            <th scope="row">
-                <label for="">Lab sayfa başlığı</label>
-            </th>
-            <td>
-                <input type="text" name="" id="" placeholder="LAB" value="" class="regular-text"/>
-            </td>
-        </tr>
-        <tr valign="top">
-            <th scope="row">
-                <label for="">Aktivite sayfa başlığı</label>
-            </th>
-            <td>
-                <input type="text" name="" id="" placeholder="AKTİVİTE" value="" class="regular-text"/>
-            </td>
-        </tr>
-        <tr valign="top">
-            <th scope="row">
-            </th>
-            <td>
-                <input type="submit" name="save_wordpresso_settings" id="submit" class="button button-primary" value="Değişiklikleri kaydet">
-            </td>
-        </tr>
-    </table>
+<form method="POST" action="">
+	<table class="form-table">
+		<tr valign="top">
+			<th scope="row">
+				<label for="wordpresso_google_analytics_track_code"><?php _e( 'Google Analytics Code', 'WordPresso' ); ?></label>
+			</th>
+			<td>
+				<input type="text" name="wordpresso_google_analytics_track_code" id="wordpresso_google_analytics_track_code" placeholder="UA-17768654-1" value="<?php echo get_option( 'wordpresso_google_analytics_track_code' ); ?>" class="regular-text" />
+			</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">
+				<label for=""><?php _e( 'Blog sayfa başlığı', 'WordPresso' ); ?></label>
+			</th>
+			<td>
+				<input type="text" name="" id="" placeholder="<?php _e( 'BLOG', 'WordPresso' ); ?>" value="" class="regular-text" />
+			</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">
+				<label for=""><?php _e( 'Lab sayfa başlığı', 'WordPresso' ); ?></label>
+			</th>
+			<td>
+				<input type="text" name="" id="" placeholder="<?php _e( 'LAB', 'WordPresso' ); ?>" value="" class="regular-text" />
+			</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">
+				<label for=""><?php _e( 'Aktivite sayfa başlığı', 'WordPresso' ); ?></label>
+			</th>
+			<td>
+				<input type="text" name="" id="" placeholder="<?php _e( 'AKTİVİTE', 'WordPresso' ); ?>" value="" class="regular-text" />
+			</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">
+			</th>
+			<td>
+				<?php submit_button( __( 'Değişiklikleri kaydet', 'WordPresso' ), 'primary', 'save_wordpresso_settings', false ); ?>
+			</td>
+		</tr>
+	</table>
 </form>
